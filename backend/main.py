5c56685e52b7382eb6ff12d2b42e0b7dfb77ce59
veermshah <veermickey@gmail.com>
2024-10-12 21:37:14 -0500

    changes

--- backend/main.py
+++ backend/main.py
@@ -38,5 +38,6 @@ def hello():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
     socketio.run(app, debug=True, host="0.0.0.0", port=5001)
--- frontend/src/app/conversate/page.tsx
+++ frontend/src/app/conversate/page.tsx
@@ -13,33 +13,26 @@ export default function page() {
             <Navbar />
 
             {started ? (
-                <main className="flex-grow p-5">
-                    <section className="text-center mb-10">
-                        <h2 className="text-3xl font-semibold">
+                <div className="flex flex-col items-center justify-center min-h-screen">
+                    <div className="bg-gray-900"></div>
+                    <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2">
+                        <div className="flex justify-center w-full mt-5">
+                            <AudioUploader />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-2xl font-bold py-2 px-4 rounded-full mt-5 text-center">
                             Improve Your Pronunciation
-                        </h2>
+                        </h1>
                         <p className="mt-2">
                             Our app analyzes your speech and provides
                             personalized feedback to help you pronounce words
                             correctly.
                         </p>
-                    </section>
-                    <section className="flex flex-col items-center mb-10">
-                        <h2 className="text-3xl font-semibold">
-                            Audio Visualizer
-                        </h2>
-                        <div className="flex justify-center w-full mt-5">
-                            <AudioUploader />
-                        </div>
-                    </section>
-                </main>
-            ) : (
-                <>
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-xl font-bold py-2 px-4 rounded-full mt-5 text-center">
-                            Start a conversation with our AI and get feedback on
-                            your pronunciation
-                        </h1>
+
                         <motion.button
                             whileHover={{ scale: 1.1 }}
                             whileTap={{ scale: 0.9 }}
