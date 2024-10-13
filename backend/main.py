e2c43e924c7561e676461926d6e6a90db617d694
veermshah <veermickey@gmail.com>
2024-10-13 03:05:32 -0500

    text-to-speech openai

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -6,6 +6,8 @@ from flask_socketio import SocketIO, emit
 import os
 import subprocess
 import audioprocess
+from openai import OpenAI
+from pathlib import Path
 
 load_dotenv()
 
@@ -14,7 +16,10 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
 client = MongoClient(os.getenv("MONGODB_URI"))
+openai_client = OpenAI()
 
+AUDIO_FOLDER = 'temp_audio'
+os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
 @socketio.on('audio_data')
 def handle_audio_data(data):
@@ -43,6 +48,51 @@ def hello():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route("/chat", methods=['POST'])
+def chat():
+    try:
+        
+        data = request.get_json()
+        user_input = data.get('text', '')
+        if not user_input:
+            return jsonify({"error": "No input provided"}), 400
+
+        response = openai_client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are friendly assitant who engages users in simple, casual conversations."},
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+        )
+        
+        assistant_reply = response.choices[0].message.content
+
+        speech_file_path = Path(__file__).parent / "speech.mp3"
+        response_ai = openai_client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=assistant_reply
+        )
+
+        response_ai.stream_to_file(speech_file_path)        
+        
+        
+
+        return jsonify({
+            "response": assistant_reply,
+            "audio": "http://localhost:5001/response.wav"
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/response.mp3', methods=['GET'])
+def get_audio():
+    return app.send_static_file('response.mp3')
 
 if __name__ == '__main__':
+    app.static_folder = os.getcwd()
     socketio.run(app, debug=True, host="0.0.0.0", port=5001)
