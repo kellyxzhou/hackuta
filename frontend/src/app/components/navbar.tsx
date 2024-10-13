c1508c4e50d9e0d0e279a846953c2e204b156a3f
Kelly Zhou <zhoukelly61@gmail.com>
2024-10-12 20:10:43 -0500

    oopsies

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
     return (
         <nav className="bg-white shadow-md text-black p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <img src="/Logo.png" alt="PronunFix" className="w-6 h-8" />
+                <img src="/Logo.png" alt="PronunFix" className="w-8 h-10" />
                 <h1 className="text-xl font-bold">PronunFix</h1>
                 <div className="space-x-4">
                     <Link href="/login" className="hover:text-green-400">
